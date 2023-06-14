eef0211b7b67550933847e63e3c7d1220b9a82b6
Jahdiel <117057573+JahdielArciniegas@users.noreply.github.com>
2023-06-13 23:47:22 -0400

    10 meses

---
+++ Js/modoOscuro.js
@@ -0,0 +1,10 @@
+const logoPage = document.querySelector('#logo')
+const logoPage1 = document.querySelector('.img-indice')
+
+logoPage.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+})
+
+logoPage1.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+})
